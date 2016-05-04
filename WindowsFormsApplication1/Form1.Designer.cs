1809a76638f35ac28cc1b4d4343333eeed1dd0b1
LaptopKU <LaptopKU@DMA>
2016-05-05 06:59:49 +0700

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class txtfrestea
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Jajan = new System.Windows.Forms.Label();
+            this.getuk = new System.Windows.Forms.Label();
+            this.bakpao = new System.Windows.Forms.Label();
+            this.steak = new System.Windows.Forms.Label();
+            this.canai = new System.Windows.Forms.Label();
+            this.pentol = new System.Windows.Forms.Label();
+            this.txtpentol = new System.Windows.Forms.MaskedTextBox();
+            this.txtsomay = new System.Windows.Forms.MaskedTextBox();
+            this.txtsteak = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox5 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox6 = new System.Windows.Forms.MaskedTextBox();
+            this.txttampilkan = new System.Windows.Forms.Button();
+            this.txthapus = new System.Windows.Forms.Button();
+            this.txtkeluar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Jajan
+            // 
+            this.Jajan.AutoSize = true;
+            this.Jajan.Location = new System.Drawing.Point(31, 4);
+            this.Jajan.Name = "Jajan";
+            this.Jajan.Size = new System.Drawing.Size(36, 13);
+            this.Jajan.TabIndex = 0;
+            this.Jajan.Text = "pentol";
+            this.Jajan.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // getuk
+            // 
+            this.getuk.AutoSize = true;
+            this.getuk.Location = new System.Drawing.Point(31, 24);
+            this.getuk.Name = "getuk";
+            this.getuk.Size = new System.Drawing.Size(37, 13);
+            this.getuk.TabIndex = 1;
+            this.getuk.Text = "somay";
+            this.getuk.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // bakpao
+            // 
+            this.bakpao.AutoSize = true;
+            this.bakpao.Location = new System.Drawing.Point(31, 44);
+            this.bakpao.Name = "bakpao";
+            this.bakpao.Size = new System.Drawing.Size(33, 13);
+            this.bakpao.TabIndex = 2;
+            this.bakpao.Text = "steak";
+            this.bakpao.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // steak
+            // 
+            this.steak.AutoSize = true;
+            this.steak.Location = new System.Drawing.Point(31, 64);
+            this.steak.Name = "steak";
+            this.steak.Size = new System.Drawing.Size(46, 13);
+            this.steak.TabIndex = 3;
+            this.steak.Text = "pok pok";
+            this.steak.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // canai
+            // 
+            this.canai.AutoSize = true;
+            this.canai.Location = new System.Drawing.Point(31, 84);
+            this.canai.Name = "canai";
+            this.canai.Size = new System.Drawing.Size(37, 13);
+            this.canai.TabIndex = 4;
+            this.canai.Text = "burger";
+            this.canai.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // pentol
+            // 
+            this.pentol.AutoSize = true;
+            this.pentol.Location = new System.Drawing.Point(31, 104);
+            this.pentol.Name = "pentol";
+            this.pentol.Size = new System.Drawing.Size(39, 13);
+            this.pentol.TabIndex = 5;
+            this.pentol.Text = "frestea";
+            this.pentol.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtpentol
+            // 
+            this.txtpentol.ForeColor = System.Drawing.Color.Yellow;
+            this.txtpentol.Location = new System.Drawing.Point(72, -3);
+            this.txtpentol.Name = "txtpentol";
+            this.txtpentol.Size = new System.Drawing.Size(100, 20);
+            this.txtpentol.TabIndex = 6;
+            this.txtpentol.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // txtsomay
+            // 
+            this.txtsomay.Location = new System.Drawing.Point(72, 19);
+            this.txtsomay.Name = "txtsomay";
+            this.txtsomay.Size = new System.Drawing.Size(100, 20);
+            this.txtsomay.TabIndex = 7;
+            // 
+            // txtsteak
+            // 
+            this.txtsteak.Location = new System.Drawing.Point(72, 37);
+            this.txtsteak.Name = "txtsteak";
+            this.txtsteak.Size = new System.Drawing.Size(100, 20);
+            this.txtsteak.TabIndex = 8;
+            // 
+            // maskedTextBox4
+            // 
+            this.maskedTextBox4.Location = new System.Drawing.Point(72, 57);
+            this.maskedTextBox4.Name = "maskedTextBox4";
+            this.maskedTextBox4.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox4.TabIndex = 9;
+            // 
+            // maskedTextBox5
+            // 
+            this.maskedTextBox5.Location = new System.Drawing.Point(72, 77);
+            this.maskedTextBox5.Name = "maskedTextBox5";
+            this.maskedTextBox5.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox5.TabIndex = 10;
+            // 
+            // maskedTextBox6
+            // 
+            this.maskedTextBox6.Location = new System.Drawing.Point(72, 97);
+            this.maskedTextBox6.Name = "maskedTextBox6";
+            this.maskedTextBox6.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox6.TabIndex = 11;
+            // 
+            // txttampilkan
+            // 
+            this.txttampilkan.Location = new System.Drawing.Point(2, 135);
+            this.txttampilkan.Name = "txttampilkan";
+            this.txttampilkan.Size = new System.Drawing.Size(75, 23);
+            this.txttampilkan.TabIndex = 12;
+            this.txttampilkan.Text = "Tampilkan";
+            this.txttampilkan.UseVisualStyleBackColor = true;
+            this.txttampilkan.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txthapus
+            // 
+            this.txthapus.Location = new System.Drawing.Point(83, 135);
+            this.txthapus.Name = "txthapus";
+            this.txthapus.Size = new System.Drawing.Size(75, 23);
+            this.txthapus.TabIndex = 13;
+            this.txthapus.Text = "Hapus";
+            this.txthapus.UseVisualStyleBackColor = true;
+            this.txthapus.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtkeluar
+            // 
+            this.txtkeluar.Location = new System.Drawing.Point(174, 135);
+            this.txtkeluar.Name = "txtkeluar";
+            this.txtkeluar.Size = new System.Drawing.Size(75, 23);
+            this.txtkeluar.TabIndex = 14;
+            this.txtkeluar.Text = "Keluar";
+            this.txtkeluar.UseVisualStyleBackColor = true;
+            this.txtkeluar.Click += new System.EventHandler(this.txtkeluar_Click);
+            // 
+            // txtfrestea
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 465);
+            this.Controls.Add(this.txtkeluar);
+            this.Controls.Add(this.txthapus);
+            this.Controls.Add(this.txttampilkan);
+            this.Controls.Add(this.maskedTextBox6);
+            this.Controls.Add(this.maskedTextBox5);
+            this.Controls.Add(this.maskedTextBox4);
+            this.Controls.Add(this.txtsteak);
+            this.Controls.Add(this.txtsomay);
+            this.Controls.Add(this.txtpentol);
+            this.Controls.Add(this.pentol);
+            this.Controls.Add(this.canai);
+            this.Controls.Add(this.steak);
+            this.Controls.Add(this.bakpao);
+            this.Controls.Add(this.getuk);
+            this.Controls.Add(this.Jajan);
+            this.Name = "txtfrestea";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Jajan;
+        private System.Windows.Forms.Label getuk;
+        private System.Windows.Forms.Label bakpao;
+        private System.Windows.Forms.Label steak;
+        private System.Windows.Forms.Label canai;
+        private System.Windows.Forms.Label pentol;
+        private System.Windows.Forms.MaskedTextBox txtpentol;
+        private System.Windows.Forms.MaskedTextBox txtsomay;
+        private System.Windows.Forms.MaskedTextBox txtsteak;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox5;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox6;
+        private System.Windows.Forms.Button txttampilkan;
+        private System.Windows.Forms.Button txthapus;
+        private System.Windows.Forms.Button txtkeluar;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class txtfrestea : Form
+    {
+        public txtfrestea()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        ("pentol :"txtpentol.TEXT&Vbnewline & _
+         "somay  :"txtsomay.Text&Vbnewline & _
+         "steak  :"txtsteak.Text&Vbnewline & _ 
+         "pokpok :"txtpokpok.Text&Vbnewline & _ 
+         "burger :"txtburger.Text&Vbnewline & _ 
+         "frestea:"txtfrestea.Text&Vbnewline & _) 
+             
+
+        private void button2_Click(object sender, EventArgs e)
+        txtpentol.clear()
+        txtsomay.clear()
+        txtsteak.clear()
+        txtpokpok.clear()
+        txtburger.clear()
+        txtfrestea.clear()
+private void txtkeluar_Click(object sender, EventArgs e)
+        txtsisa.Text= Val(txtkasmasuk.Text)-Val(txtkaskeluar.Txt)
+
+        
+    
+    
+
